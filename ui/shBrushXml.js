4c182bbd3cf83df3eff4daa30ee01fb795db0f3a
naturtle <692425987@qq.com>
2016-05-12 22:37:39 +0800

    Add files via upload

--- ui/shBrushXml.js
+++ ui/shBrushXml.js
@@ -102,15 +102,75 @@
                 orderLine=commentMatch[0].substring(beginIndex,commentMatch[0].length)
                 ;
             result.push(
-                new constructor(order, commentMatch.index +beginIndex+ orderLine.indexOf(order), 'color1')
+                new constructor(order, commentMatch.index +beginIndex+ orderLine.indexOf(order), 'color4')
             );
             
-            result.push(
-                new constructor(condition, commentMatch.index+beginIndex + orderLine.indexOf(condition), 'string')
-            );
+            if(condition){
+                if(order==="scope"||order==="-"){
+                    condition=splitByOnce(condition,"|")[1];
+                }
+                var c=[],r;
+                for(var i=0;i<jsRegexList.length;i++){
+                    r=y(condition,jsRegexList[i]);
+                    if(r.length){
+                        c=c.concat(r);    
+                    }
+                }
+                var idx=commentMatch.index+beginIndex + orderLine.indexOf(condition);
+                for(var i=0;i<c.length;i++){
+                    c[i].index+=idx;
+                }
+                result=result.concat(c); 
+            }
+            
 
             return result;
         }
+        function splitByOnce(s,split){
+            var 
+                index=s.indexOf(split),
+                arr=[];
+            if(index!=-1){
+                arr.push(s.substring(0,index));
+                arr.push(s.substring(index+split.length,s.length));
+            }else{
+                arr.push(s);
+            }
+            return arr;
+        }
+        function c(k) {
+            return k[0]
+        }
+        function y(a, b) {
+            for (var d = null, h = [], g = b.func ? b.func : c; (d = b.regex.exec(a)) != null; ) {
+                var i = g(d, b);
+                if (typeof i == "string")
+                    i = [new SyntaxHighlighter.Match(i, d.index, b.css)];
+                h = h.concat(i)
+            }
+            return h
+        }
+        var keywords =  'break case catch continue ' +
+                        'default delete do else false  ' +
+                        'for function if in instanceof ' +
+                        'new null return super switch ' +
+                        'this throw true try typeof ' +
+                        'var while with debugger console'
+                        ;
+
+        var r = SyntaxHighlighter.regexLib;
+        var jsRegexList = [
+            { regex: r.multiLineDoubleQuotedString,                             css: 'string' },            // double quoted strings
+            { regex: r.multiLineSingleQuotedString,                             css: 'string' },            // single quoted strings
+            { regex: r.singleLineCComments,                                     css: 'comments' },          // one line comments
+            { regex: r.multiLineCComments,                                      css: 'comments' },          // multiline comments
+            { regex: /\s*#.*/gm,                                                css: 'preprocessor' },      // preprocessor tags like #region and #endregion
+            { regex: new RegExp(this.getKeywords(keywords), 'gm'),              css: 'string' },            // keywords
+            { regex: /\={1,3}|\{|\}|\(|\)|\[|\]|\.|\||\:|\?|&gt;|&lt;|;|,|\+|\-|\/|\*/g,          css: 'keyword2' }           // keywords
+            ];
+    
+        
+        
 		this.regexList = [
 		    { regex: noOrderRE,                                                                             func: comment },    // <! ... turtle order>
 			{ regex: new XRegExp('(\\&lt;|<)\\!\\[[\\w\\s]*?\\[(.|\\s)*?\\]\\](\\&gt;|>)', 'gm'),           css: 'color2' },    // <![ ... [ ... ]]>
